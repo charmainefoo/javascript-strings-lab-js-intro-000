626715e50a962f3ad0ac8029613d391cd1cef7f6
Charmaine Foo <vincybrat@gmail.com>
2018-09-23 06:39:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "hello" + "Neil deGrasse Tyson" + "!";
+var greetSpecialGuest = "hello," + "Neil deGrasse Tyson" + "!";
 
 var topic = "how are you?";
 
